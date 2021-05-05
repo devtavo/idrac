4dbfd20a218eab78da8e0988414e52fd77be8eb2
Luis Madueño <gustavoreyesz@hotmail.com>
2021-05-05 17:26:09 -0500

    integracion con FB pixel

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
     <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
     <script src="js/html5shiv.min.js"></script>
     <![endif]-->
+    <!-- Facebook Pixel Code -->
     <script>
         ! function(f, b, e, v, n, t, s) {
             if (f.fbq) return;
@@ -3302,7 +3303,7 @@
     <!-- coded by Himic-->
 
     <!-- Facebook Pixel Code -->
-    
+
 
 
 </body>
